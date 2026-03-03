d72da46541c7e61cb35f234471bbb09bcd17d934
Hassan Daud <hd0789529@gmail.com>
2026-03-03 23:50:44 +0500

    refactor: langgraph backend.ppy

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -1,29 +1,20 @@
 from langgraph.graph import StateGraph, START, END
 from langchain_google_genai import ChatGoogleGenerativeAI
-from typing import TypedDict, Literal
+from typing import TypedDict, Literal, Annotated
 from dotenv import load_dotenv
 from pydantic import BaseModel, Field
 from langgraph.checkpoint.memory import InMemorySaver
 from langchain_core.messages import BaseMessage, SystemMessage, HumanMessage
+from langgraph.graph.message import add_messages 
+
 
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
-from langgraph.graph import StateGraph, START, END
-from langchain_google_genai import ChatGoogleGenerativeAI
-from typing import TypedDict, Literal, Annotated
-from dotenv import load_dotenv
-from pydantic import BaseModel, Field
-from langchain_core.messages import BaseMessage,AIMessage, SystemMessage, HumanMessage
-from langgraph.graph.message import add_messages 
-from langgraph.checkpoint.memory import MemorySaver 
-
-
 class ChatState(TypedDict) :
     messages: Annotated[list[BaseMessage], add_messages]
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 def chat_node(state: ChatState) :
     # Take user query 
     messages = state['messages']
@@ -32,7 +23,7 @@ def chat_node(state: ChatState) :
     # Save the response in state
     return {'messages' : [response]}
 
-checkpointer = MemorySaver()
+checkpointer = InMemorySaver()
 
 graph = StateGraph(ChatState)
 graph.add_node('chat_node',chat_node)
